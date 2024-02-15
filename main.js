d04e7774b3e762967ac215b4cddf917cf4813f73
Yoonjae_Moon <yoonjaemoon20@gmail.com>
2024-02-15 23:04:17 +0900

    main

--- main.js
+++ main.js
@@ -11,8 +11,22 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div")
 let taskList = []
+let filterList = []
 addButton.addEventListener("click", addTask)
+let mode = 'all'
+
+for (let i = 1 ; i < tabs.length ; i++){
+    tabs[i].addEventListener("click", function(event){
+        filter(event)})
+}
+
+taskInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
 
 function addTask(){
     let taskContent = taskInput.value;
@@ -29,9 +43,21 @@ function addTask(){
 }
 
 function render(){
+    // 1. 내가 선택한 탭에 따라서 
+    let list = []
+    if (mode === "all"){
+        list = taskList;
+    }
+    else{
+        list = filterList;
+    }
+    // 2. 리스트를 달리 보여준다.
+
+    // all -> taskList
+    // ongoing, done -> filterList
     let resultHTML ='';
-    for(let i = 0; i < taskList.length; i++){
-        let task = taskList[i];
+    for(let i = 0; i < list.length; i++){
+        let task = list[i];
         resultHTML += 
             `<div class="task">
                 <div class="${task.isComplete ? 'task-done' : ''}">
@@ -70,8 +96,41 @@ function deleteTask(id){
         }
     }
     render();
+    filter({target: {id:mode}});
 }
 
+function filter(event){
+    mode = event.target.id;
+    filterList= [];
+
+    if(mode === "all"){
+        //전체 리스트를 보여준다.
+        render();
+    }else if (mode === "ongoing"){
+        //진행중인 아이템을 보여준다
+        //task.isComplete=false
+        for (let i = 0 ; i < taskList.length ; i++){
+            if (taskList[i].isComplete === false){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }else if (mode === "done"){
+        //끝나는 케이스
+        //task.isComplete = true
+        for (let i = 0 ; i < taskList.length ; i++){
+            if (taskList[i].isComplete === true){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }
+    
+    let tabWidth = event.target.offsetWidth;
+    let tabOffset = event.target.offsetLeft;
+    document.getElementById("under-line").style.width = `${tabWidth}px`;
+    document.getElementById("under-line").style.left = `${tabOffset}px`;
+}
 
 function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2,9)
